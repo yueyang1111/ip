ed6cba2a1459d53a539651d15d4e35066485d0ec
yueyang1111 <tanyueyang03@gmail.com>
2026-01-21 22:54:41 +0800

    Rename Duke to Edith; implement greeting

---
+++ src/main/java/Edith.java
@@ -0,0 +1,10 @@
+public class Edith {
+    public static void main(String[] args) {
+        System.out.println("___________________________________");
+        System.out.println("Hello! I'm Edith");
+        System.out.println("What can I do for you?");
+        System.out.println("___________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("___________________________________");
+    }
+}
