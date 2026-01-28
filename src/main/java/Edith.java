27eeec2c937c2d9b26a21245d92e2563dca5526a
Tan Yue Yang <tanyueyang03@gmail.com>
2026-01-29 00:22:03 +0800

    Implement add and list commands

--- src/main/java/Edith.java
+++ src/main/java/Edith.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class Edith {
     public static void main(String[] args) {
         String line = "    ___________________________________";
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         System.out.println(line);
         System.out.println("    Hello! I'm Edith");
         System.out.println("    What can I do for you?");
@@ -17,8 +20,14 @@ public class Edith {
                 System.out.println("    Bye. Hope to see you again soon!");
                 System.out.println(line);
                 break;
+            } else if (userInput.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println("    " + (i + 1) + ". " + tasks[i]);
+                }
             } else {
-                System.out.println("    " + userInput);
+                tasks[taskCount] = userInput;
+                taskCount++;
+                System.out.println("    added: " + userInput);
             }
             System.out.println(line);
         }
