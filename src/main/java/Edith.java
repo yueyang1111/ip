0dce39ec68209c7baaf73382b90cca972d1bef85
Tan Yue Yang <tanyueyang03@gmail.com>
2026-01-29 00:03:07 +0800

    Implement echo feature

--- src/main/java/Edith.java
+++ src/main/java/Edith.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Edith {
     public static void main(String[] args) {
-        System.out.println("___________________________________");
-        System.out.println("Hello! I'm Edith");
-        System.out.println("What can I do for you?");
-        System.out.println("___________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("___________________________________");
+        String line = "    ___________________________________";
+        System.out.println(line);
+        System.out.println("    Hello! I'm Edith");
+        System.out.println("    What can I do for you?");
+        System.out.println(line);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String userInput = scanner.nextLine();
+            System.out.println(line);
+            if (userInput.equals("bye")) {
+                System.out.println("    Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            } else {
+                System.out.println("    " + userInput);
+            }
+            System.out.println(line);
+        }
+        scanner.close();
     }
 }
