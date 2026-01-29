8a7dad239d96c9b8ec7712a7fa72dd88b75e69b3
Tan Yue Yang <tanyueyang03@gmail.com>
2026-01-29 12:55:05 +0800

    Implement mark and unmark features

--- src/main/java/Edith.java
+++ src/main/java/Edith.java
@@ -1,36 +1,82 @@
 import java.util.Scanner;
 
 public class Edith {
+    private static final String LINE = "    ___________________________________";
+
     public static void main(String[] args) {
-        String line = "    ___________________________________";
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
-        System.out.println(line);
-        System.out.println("    Hello! I'm Edith");
-        System.out.println("    What can I do for you?");
-        System.out.println(line);
-
+        printGreeting();
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
             String userInput = scanner.nextLine();
-            System.out.println(line);
             if (userInput.equals("bye")) {
-                System.out.println("    Bye. Hope to see you again soon!");
-                System.out.println(line);
+                printExitMessage();
                 break;
             } else if (userInput.equals("list")) {
-                for (int i = 0; i < taskCount; i++) {
-                    System.out.println("    " + (i + 1) + ". " + tasks[i]);
-                }
+                printTaskList(tasks, taskCount);
+            } else if (userInput.startsWith("mark ")) {
+                markTask(tasks, userInput);
+            } else if (userInput.startsWith("unmark ")) {
+                unmarkTask(tasks, userInput);
             } else {
-                tasks[taskCount] = userInput;
+                addTask(tasks, taskCount, userInput);
                 taskCount++;
-                System.out.println("    added: " + userInput);
             }
-            System.out.println(line);
         }
         scanner.close();
+    }
+
+    private static void printGreeting() {
+        System.out.println(LINE);
+        System.out.println("    Hello! I'm Edith");
+        System.out.println("    What can I do for you?");
+        System.out.println(LINE);
+    }
+
+    private static void printExitMessage() {
+        System.out.println(LINE);
+        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.println(LINE);
+    }
+
+    private static void printTaskList(Task[] tasks, int taskCount) {
+        System.out.println(LINE);
+        System.out.println("    Here are the tasks in your list:");
+        for (int i = 0; i < taskCount; i++) {
+            Task task = tasks[i];
+            System.out.println("    " + (i + 1) + ".[" + task.getStatusIcon()
+                    + "] " + task.getDescription());
+        }
+        System.out.println(LINE);
+    }
+
+    private static void addTask(Task[] tasks, int taskCount, String userInput) {
+        System.out.println(LINE);
+        tasks[taskCount] = new Task(userInput);
+        System.out.println("    added: " + userInput);
+        System.out.println(LINE);
+    }
+
+    private static void markTask(Task[] tasks, String userInput) {
+        System.out.println(LINE);
+        int index = Integer.parseInt(userInput.split(" ")[1]) - 1;
+        Task task = tasks[index];
+        task.markAsDone();
+        System.out.println("    Nice! I've marked this task as done:");
+        System.out.println("    [X] " + task.getDescription());
+        System.out.println(LINE);
+    }
+
+    private static void unmarkTask(Task[] tasks, String userInput) {
+        System.out.println(LINE);
+        int index = Integer.parseInt(userInput.split(" ")[1]) - 1;
+        Task task = tasks[index];
+        task.markAsNotDone();
+        System.out.println("    OK, I've marked this task as not done yet:");
+        System.out.println("    [ ] " + task.getDescription());
+        System.out.println(LINE);
     }
 }
